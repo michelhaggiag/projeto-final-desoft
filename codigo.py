8c36c42232f5d9c4a88118ffc3306bcb447dc756
lucabp <lucabp@al.insper.edu.br>
2018-11-22 16:31:12 -0200

    Tela inicial

--- codigo.py
+++ codigo.py
@@ -10,7 +10,14 @@ import sys
 from pygame.locals import *
 from random import randrange
 
+preto=(0,0,0)
+branco=(225,225,225)
+altura = 800
+largura = 600
+
 # ===============      CLASSES      ===============
+gameDisplay = pygame.display.set_mode((largura,altura))
+
 class nave(pygame.sprite.Sprite):
     def __init__(self, arquivo_imagem, pos_x, pos_y, vel_x):
         pygame.sprite.Sprite.__init__(self)
@@ -63,11 +70,12 @@ class vidas(pygame.sprite.Sprite):
 
 #class tiro_inimigo:
 #    def__init__(self,pos_x,pos_y)
-
+def text_objects(text, font):
+    textSurface = font.render(text, True, branco)
+    return textSurface, textSurface.get_rect()
 
 relogio = pygame.time.Clock()
 
-branco=(225,225,225)
  
  
 # ===============   INICIALIZAÇÃO   ===============        
@@ -75,8 +83,7 @@ pygame.init()
 tela = pygame.display.set_mode((800, 600), 0, 32)
 pygame.display.set_caption('Space Invaders')
 
-altura = 800
-largura = 600
+
 
 fundo = pygame.image.load("800x600.jpg").convert()
 
@@ -118,7 +125,19 @@ def Pontuacao(score):
     texto = fonte.render("Score: "+str(score),True, branco)
     tela.blit(texto,(0,0))
     
-
+def tela_inicial():
+    
+    tela.fill(preto)
+    largeText = pygame.font.Font('freesansbold.ttf',90)
+    largeText2 = pygame.font.Font('freesansbold.ttf',30)
+    TextSurf, TextRect = text_objects("Space Invaders", largeText)
+    TextSurf2, TextRect2 = text_objects("(Pressione qualquer tecla para continuar)", largeText2)
+    TextRect.center = (((altura/2)),(largura/2))
+    TextRect2.center = (((altura/2)),((largura/2)+60))      
+    gameDisplay.blit(TextSurf, TextRect)
+    gameDisplay.blit(TextSurf2, TextRect2)
+    pygame.display.update()
+    relogio.tick(30)
 
 
 
@@ -140,92 +159,106 @@ def Pontuacao(score):
 
 
 
+estado=0
 
 
 rodando = True
 while rodando:
     tempo = relogio.tick(30)
-    
-    if pygame.sprite.groupcollide(tiro_group, inimigo_group, True, True, collided = None):
-        score+=1
-    # === PRIMEIRA PARTE: LIDAR COM EVENTOS ===
-
-    # Para cada evento não-processado na lista de eventos:
+    pressed_keys = pygame.key.get_pressed() 
     for event in pygame.event.get():
-        # Verifica se o evento atual é QUIT (janela fechou).
+    # Verifica se o evento atual é QUIT (janela fechou).
         if event.type == QUIT:
             # Neste caso, marca o flag rodando como False, 
             # para sair do loop de jogo.
             rodando = False
-    pressed_keys = pygame.key.get_pressed() 
-    if pressed_keys[K_RIGHT]:  
-        nave.rect.x += 10
-    elif pressed_keys[K_LEFT]:  
-        nave.rect.x -= 10
-    elif pressed_keys[K_SPACE]:
-        tiro = Tiro('tiro.jpg',((nave.rect.x)+57),((nave.rect.y)+40),-10)
-        tiro_group.add(tiro)
-        for tiro in tiro_group:
-            if tiro.rect.y > altura:
-                tiro.kill()    
-    if nave.rect.x > 675:
+    if estado == 0:
+        if any(pressed_keys):
+            estado = 1
+        tela_inicial()
+        
+    elif estado == 1:
+  
+        if pygame.sprite.groupcollide(tiro_group, inimigo_group, True, True, collided = None):
+            score+=1
+        # === PRIMEIRA PARTE: LIDAR COM EVENTOS ===
+    
+        # Para cada evento não-processado na lista de eventos:
+        for event in pygame.event.get():
+            # Verifica se o evento atual é QUIT (janela fechou).
+            if event.type == QUIT:
+                # Neste caso, marca o flag rodando como False, 
+                # para sair do loop de jogo.
+                rodando = False
+        pressed_keys = pygame.key.get_pressed() 
         if pressed_keys[K_RIGHT]:  
-             nave.rect.x =675
-#             tiro.rect.x =675
-    elif nave.rect.x < 0:
-        if pressed_keys[K_LEFT]:  
-            nave.rect.x =0
-#            tiro.rect.x =0
-#    if inimigo1.rect.x > 675:
-#        inimigo1.rect.x = 675 
-#        inimigo1.rect.y += 35
-#        inimigo1.vx = -10
-#    elif inimigo1.rect.x < 0:
-#        inimigo1.rect.x = 0
-#        inimigo1.rect.y+=35
-#        inimigo1.vx = 10
-#    if inimigo1.rect.y > 420:
-#        nave.kill()
-#        inimigo1.kill()
-#    elif inimigo1.rect.y > 530:
-#        inimigo1.kill()
-#    if inimigo2.rect.y > 530:
-#        nave.kill()
-#        inimigo1.kill()
-#        inimigo2.kill()
+            nave.rect.x += 10
+        elif pressed_keys[K_LEFT]:  
+            nave.rect.x -= 10
+        elif pressed_keys[K_SPACE]:
+            tiro = Tiro('tiro.jpg',((nave.rect.x)+57),((nave.rect.y)+40),-10)
+            tiro_group.add(tiro)
+            for tiro in tiro_group:
+                if tiro.rect.y > altura:
+                    tiro.kill()    
+        if nave.rect.x > 675:
+            if pressed_keys[K_RIGHT]:  
+                 nave.rect.x =675
+    #             tiro.rect.x =675
+        elif nave.rect.x < 0:
+            if pressed_keys[K_LEFT]:  
+                nave.rect.x =0
+    #            tiro.rect.x =0
+    #    if inimigo1.rect.x > 675:
+    #        inimigo1.rect.x = 675 
+    #        inimigo1.rect.y += 35
+    #        inimigo1.vx = -10
+    #    elif inimigo1.rect.x < 0:
+    #        inimigo1.rect.x = 0
+    #        inimigo1.rect.y+=35
+    #        inimigo1.vx = 10
+    #    if inimigo1.rect.y > 420:
+    #        nave.kill()
+    #        inimigo1.kill()
+    #    elif inimigo1.rect.y > 530:
+    #        inimigo1.kill()
+    #    if inimigo2.rect.y > 530:
+    #        nave.kill()
+    #        inimigo1.kill()
+    #        inimigo2.kill()
+            
+        tiro_group.update()
+        inimigo_group.update()
+    
         
-    tiro_group.update()
-    inimigo_group.update()
-
-    
-    
+        
+            
+        
+    # Check the list of colliding sprites, and add one to the score for each one
+        #for block in inimigo_group_list:
+         #   score +=1 
         
     
-# Check the list of colliding sprites, and add one to the score for each one
-    #for block in inimigo_group_list:
-     #   score +=1 
+            
+    #    tiro.move()
+    #    nave.move()
     
-
+        # === FIM DA SEGUNDA PARTE ===
+    
+        # === TERCEIRA PARTE: GERA SAÍDAS (pinta tela, etc) ===
+    
+        # Pinta a imagem de fundo na tela auxiliar.
+        tela.blit(fundo, (0, 0))                
+        Pontuacao(score)
         
-#    tiro.move()
-#    nave.move()
-
-    # === FIM DA SEGUNDA PARTE ===
-
-    # === TERCEIRA PARTE: GERA SAÍDAS (pinta tela, etc) ===
-
-    # Pinta a imagem de fundo na tela auxiliar.
-    tela.blit(fundo, (0, 0))                
-    Pontuacao(score)
+        nave_group.draw(tela)
+        tiro_group.draw(tela)
+        inimigo_group.draw(tela)
     
-    nave_group.draw(tela)
-    tiro_group.draw(tela)
-    inimigo_group.draw(tela)
-
-    # Troca de tela na janela principal.
-    pygame.display.update()
-
-    # === FIM DA TERCEIRA PARTE ===
-    # Agora volta para o início do loop e faz mais um passo do jogo.
+        # Troca de tela na janela principal.
+        pygame.display.update()
+    
+        # === FIM DA TERCEIRA PARTE ===
+        # Agora volta para o início do loop e faz mais um passo do jogo.
 pygame.display.quit()
 
